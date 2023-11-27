d093301d2cf0d9866aee39881021238929d64b62
PerflexEr <stepasorokin56@gmail.com>
2023-11-27 14:12:27 +0200

    final routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
+import React, {useContext, useEffect, useState} from 'react';
+import {BrowserRouter} from "react-router-dom";
+import AppRouter from "./components/Approuter";
+import {observer} from "mobx-react-lite";
+import {Context} from "./index";
+// import {check} from "./http/userAPI";
 
-
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
-}
+const App = observer(() => {
+    return (
+        <BrowserRouter>
+            <AppRouter />
+        </BrowserRouter>
+    );
+});
 
 export default App;
--- client/src/components/Approuter.jsx
+++ client/src/components/Approuter.jsx
@@ -1,24 +1,23 @@
 import React, { useContext } from 'react';
-import { Routes, Route, Navigate} from "react-router-dom";
-import { authRoutes, publicRoutes ,  } from '../routes';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { authRoutes, publicRoutes } from '../routes';
 import { Context } from "../index";
-const AppRouter = () => {
 
-    const {user} = useContext(Context)
-    console.log(user);
+const AppRouter = () => {
+    const { user } = useContext(Context);
+    console.log(user._isAuth);
+
     return (
         <Routes>
-            {user.isAuth === true &&
-                authRoutes.map(({ path, Element }) => (
-                    <Route key={path} path={path} element={<Element />} />
+            {user._isAuth === true &&
+                authRoutes.map(({ path, Component }) => (
+                    <Route key={path} path={path} element={<Component />} />
                 ))
             }
-            {
-                publicRoutes.map(({ path, Element }) => (
-                    <Route key={path} path={path} element={<Element />} />
-                ))
-            }
-            <Route path="*" element={<Navigate to="/" replace />}/>
+            {publicRoutes.map(({ path, Component }) => (
+                <Route key={path} path={path} element={<Component />} />
+            ))}
+            <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
     );
 };
