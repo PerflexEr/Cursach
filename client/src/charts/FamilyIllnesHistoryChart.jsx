d94f4a5479fddead423a6729c6da9653f21206a3
PerflexEr <stepasorokin56@gmail.com>
2023-11-30 21:50:43 +0200

    charts

---
+++ client/src/charts/FamilyIllnesHistoryChart.jsx
@@ -0,0 +1,79 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { observer } from 'mobx-react-lite';
+import { Context } from '../index';
+
+import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Title, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(ArcElement, Title, Tooltip, Legend);
+
+const FamilyIllnesHistoryChart = observer(() => {
+
+  const { medicines, illnes } = useContext(Context);
+
+
+  useEffect(() => {
+    medicines.fetchMedicines();
+    illnes.fetchIllneses()
+  }, [medicines , illnes]);
+
+
+  let unusedMedicines = 0
+  let usedMedicines = 0
+  let expiredMedicines = 0
+
+  medicines._medicinesWithIdAndNameAndExpDate.map((medicine) => {
+    if (new Date(medicine.expDate) <= new Date()) {
+     expiredMedicines++;
+    }
+  })
+
+  const medicineInBalance = illnes._illneses.map((illnes) => ({
+    medicineId: illnes.MedicineId,
+    amountUsed: illnes.amount_of_pills,
+  }));
+
+  let matchingMedicine;
+
+  medicines._medicinesWithIdAndNameAndExpDate.map((medicine) => {
+      matchingMedicine = medicineInBalance.find((balance) => balance.medicineId === medicine.id);
+      if (matchingMedicine) {
+        usedMedicines++;
+      }
+  });
+
+  unusedMedicines = medicines._medicinesWithIdAndNameAndExpDate.length - (usedMedicines + expiredMedicines)
+
+
+  const data = {
+  labels: ['Unused', 'Already used', 'Expired'],
+  datasets: [
+    {
+      data: [unusedMedicines, usedMedicines, expiredMedicines],
+      backgroundColor: [
+        'rgba(75, 192, 192, 0.5)',
+        'rgba(255, 206, 86, 0.5)',
+        'rgba(255, 99, 132, 0.5)',
+      ],
+    },
+  ],
+};
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Medicine Usage Chart',
+      },
+    },
+  };
+
+  return <Pie data={data} options={options} />;
+});
+
+export default FamilyIllnesHistoryChart;
--- client/src/pages/Statistics.jsx
+++ client/src/pages/Statistics.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import MedicinesChart from '../charts/MedicinesChart';
-import FamilyAgeGroups from '../charts/FamilyAgeGroups';
-import FamilyMemberSpent from '../charts/FamilyMemberSpent';
+import MedicinesChart from '../charts/MedicinesChartChart';
+import FamilyAgeGroups from '../charts/FamilyAgeGroupsChart';
+import FamilyMemberSpent from '../charts/FamilyMemberSpentChart';
+import FamilyIllnesHistoryChart from '../charts/FamilyIllnesHistoryChart';
+
+import { observer } from 'mobx-react-lite';
+
 import { Container } from '@mui/material';
-const Statistics = () => {
+
+const Statistics = observer(() => {
   return (
     <Container>
       <div style={{ height: '400px' }}> 
         <MedicinesChart />
         <FamilyAgeGroups/>
         <FamilyMemberSpent/>
+        <FamilyIllnesHistoryChart/>
       </div>
     </Container>
   );
-};
+});
 
 export default Statistics;
